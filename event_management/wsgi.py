88c010c3dfef051304500baad05eaba8433eb854
amitk2003 <125468413+amitk2003@users.noreply.github.com>
2024-05-19 17:36:04 +0530

    Update wsgi.py

--- event_management/wsgi.py
+++ event_management/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_management.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amit_innovators.settings')
 
 application = get_wsgi_application()
-app=application
+
